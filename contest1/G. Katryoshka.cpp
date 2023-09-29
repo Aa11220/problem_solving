60e8c7830b89fdd2b30ab29702f31df147cb29a9
Aa11220 <120989724+Aa11220@users.noreply.github.com>
2023-09-28 18:11:25 -0700

    Update G. Katryoshka.cpp
    add comment

--- contest1/G. Katryoshka.cpp
+++ contest1/G. Katryoshka.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+//here we ignore the 2nd constrain caz if you only have one eye it can make a statue so why waste 2 in one statue 
+//about the anser we here see min from the components and remove it from each component to make min number of status 
+//then turn to frist condation caz you here have to use 2 eyes so you make the min between body and half numbers of eyes
 int main(int argc, char const *argv[])
 {
     long long eye , mouth,bodyes ,results = 0;
