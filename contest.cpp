9fa591540d9d271da23e627dfc70ab087e5ebe9e
unknown <ahmedtarek200a@gmail.com>
2023-09-28 17:10:37 -0700

    F. Adding Bits

--- contest.cpp
+++ contest.cpp
@@ -4,6 +4,7 @@ int main(int argc, char const *argv[])
 {
     int frist,second,result;
     cin>>frist>>second;
+    //here if you see the two binary numbers you can see xor oper
     result = frist ^ second;
     return 0;
 }
