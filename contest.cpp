4214ce18353fd0cdf943083437358ef7e2ca5575
unknown <ahmedtarek200a@gmail.com>
2023-09-28 17:09:11 -0700

    F. Adding Bits

---
+++ contest.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int frist,second,result;
+    cin>>frist>>second;
+    result = frist ^ second;
+    return 0;
+}
